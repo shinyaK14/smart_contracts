d9b6e9b35e34fbfa7b19345268bf831dc71dfb2f
Shinya Kitamura <9897663+shinyaK14@users.noreply.github.com>
2022-05-20 21:01:18 +0200

    initial

---
+++ erc1155_cryplo.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "hardhat/console.sol";
+
+contract NFTCryplo is ERC1155, Ownable {
+  mapping(uint256 => address) private minters;
+
+  constructor() ERC1155(""){}
+
+  uint MINT_PRICE = 1 ether;
+  uint PREMIUM_MINT_PRICE = 5 ether;
+
+  function contractURI() public pure returns (string memory) {
+    return "https://cryplo-api.herokuapp.com/contract_metadata.json";
+  }
+
+  function mint(address account,uint256 id,uint256 amount) public {
+    require(minters[id] == address(0) || minters[id] == msg.sender);
+    if(minters[id] == address(0)) {
+      minters[id] = msg.sender;
+    }
+    _mint(account,id,amount,"");
+  }
+
+  function bulkMint(address account, uint256[] memory ids, uint256[] memory amount) public payable {
+    if(ids.length > 2) {
+      require(msg.value >= PREMIUM_MINT_PRICE, "Not enough MATIC");
+
+      mint(account, ids[0], amount[0]);
+      mint(account, ids[1], amount[1]);
+      mint(account, ids[2], amount[2]);
+    } else if(ids.length > 1) {
+      require(msg.value >= MINT_PRICE, "Not enough MATIC");
+
+      mint(account, ids[0], amount[0]);
+      mint(account, ids[1], amount[1]);
+    }else if(ids.length > 0) {
+      mint(account, ids[0], amount[0]);
+    }
+  }
+
+  function burn(address account,uint256 id,uint256 amount) public {
+    require(msg.sender == account);
+    _burn(account,id,amount);
+  }
+
+  function uri(uint256 _id) override public pure returns (string memory) {
+    return string(
+        abi.encodePacked(
+          "https://cryplo-api.herokuapp.com/nft/metadatas/",
+          Strings.toString(_id)
+          )
+        );
+  }
+
+  function getBalance() public view returns(uint) {
+    return address(this).balance;
+  }
+
+  function withdraw() onlyOwner public payable {
+    payable(owner()).transfer(address(this).balance);
+  }
+
+  function selfDestruct() onlyOwner public payable {
+    selfdestruct(payable(owner()));
+  }
+}
+
+// https://docs.openzeppelin.com/contracts/4.x/api/access#AccessControl
+//https://rinkeby.rarible.com/token/polygon/0x21c5FE54806cB616e3398dBBA94341F43afaB73b:2?tab=owners
+
+
+
