6824c98b397a2ecc9d9de3e86b211d10208f3266
Shinya Kitamura <9897663+shinyaK14@users.noreply.github.com>
2022-06-16 18:31:26 +0200

    update rec20

--- erc20_cryplo.sol
+++ erc20_cryplo.sol
@@ -1,10 +1,21 @@
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+// SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 pragma solidity ^0.8.0;
 
-contract Token is ERC20 {
+contract Token is ERC20, Ownable {
   constructor () public ERC20("Jabba test Token", "JBT") {
     _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
   }
+
+  function bulksendToken(address[] memory _to, uint256[] memory _values) onlyOwner public payable {
+    require(_to.length == _values.length);
+
+    for (uint256 i = 0; i < _to.length; i++) {
+      transfer( _to[i], _values[i]);
+    }
+  }
 }
+
