799b02dc4084677f7119fd07773f153a3137fd56
Shinya Kitamura <9897663+shinyaK14@users.noreply.github.com>
2022-06-17 09:18:48 +0200

    update erc20

--- erc20_cryplo.sol
+++ erc20_cryplo.sol
@@ -6,15 +6,15 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 pragma solidity ^0.8.0;
 
 contract CryploToken is ERC20, Ownable {
-  constructor () public ERC20("Jabba test Token", "JBT") {
-    _mint(msg.sender, 1000000 * (10 ** uint256(decimals())));
+  constructor () public ERC20("Jabba test Token", "CRP") {
+    _mint(msg.sender, 100000000 * (10 ** uint256(decimals())));
   }
 
   function bulksendToken(address[] memory _to, uint256[] memory _values) onlyOwner public payable {
     require(_to.length == _values.length);
 
     for (uint256 i = 0; i < _to.length; i++) {
-      transfer( _to[i], _values[i]);
+      transfer( _to[i], _values[i] *(10**18));
     }
   }
 }
