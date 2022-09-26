3c2ffb0d3cdd469ec32976a559809deef0b6c7c9
Shinya Kitamura <9897663+shinyaK14@users.noreply.github.com>
2022-09-26 16:00:38 +0200

    update

--- deblog_opensea_nft.sol
+++ deblog_opensea_nft.sol
@@ -75,7 +75,7 @@ contract DeblogNFT is ERC1155, Ownable {
     bytes memory data
   ) internal override {
     for(uint256 i=0; i < ids.length; i++){
-      require(minters[ids[i]] == from);
+      require(minters[ids[i]] == from || minters[ids[i]] == to);
     }
     super._beforeTokenTransfer(operator, from, to, ids, amounts, data);
   }
